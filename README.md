ba6645879871b714f2d39e26fe29014a6501deee
R-thinking <Making.R.W@gmail.com>
2021-12-13 00:37:14 +0900

    README:Upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Shopping List
+
+## Index
+
+> 1.  [Description](#Description)
+> 2.  [UI Design](#UI-Design)
+> 3.  [Update List](#Update-List)
+
+<br/>
+
+## Description
+
+> It's Shopping List mobile UI to add or remove Items.
+
+<br/>
+
+## UI Design
+
+<table cellspacing="0">
+  <tr align="center">
+    <td colspan="1"> <b>Responsive Design</b> </td>
+  </tr>
+  <td align="center"> <img src="/img/UI.png" width="768" height="550"/> <span>  768px or bigger </span> </td>
+</table>
+  <br/>
+
+### Update List
+
+- [x] To build basic structure like **youtube** using html
+- [x] To design a web page like **youtube** using css
+- [x] To apply **Responsive Web** to the design.
+- [x] To add simple functions by JavaScript
--- main.js
+++ main.js
@@ -3,17 +3,32 @@
 const inputForm = document.querySelector('form');
 const input__item = document.querySelector('.input__item');
 const items = document.querySelector('.items');
+const addBtn = document.querySelector('.addBtn');
 
-inputForm.addEventListener('submit', (e)=> {
+// sol1
+// inputForm.addEventListener('submit', (e)=> {
+//     e.preventDefault();
+//     displayItem(input__item.value);
+//     removeItem();
+// });
+
+//sol2
+addBtn.addEventListener('click',(e) => {
     e.preventDefault();
-    displayItem(input__item.value);
-    removeItem();
+    onAdd();
+});
+
+input__item.addEventListener('keypress',(event)=> {
+    if(event==='Enter'){
+        onAdd();
+    }
 });
 
 
 
+//function
 
-
+//sol1. function
 function displayItem(item){
     items.innerHTML += `
     <div class="item">
@@ -37,5 +52,44 @@ function removeItem() {
     }
 }
 
+//sol2. function
+function onAdd() {
+    //1. fetching text user input.
+    const text = input__item.value;
+    if(text===''){
+        input__item.focus();
+        return;
+    }
+    //2. create new item.
+    const item = createItem(text);
+    //3. add item in items.
+    items.appendChild(item);
+    // 4. scroll to the added item.
+    item.scrollIntoView({behavior:"smooth"});
+    //5. initialize input.
+    input__item.value = '';
+    input__item.focus();
+}
+
+function createItem(text) {
+    const item = document.createElement('div');
+    item.setAttribute('class','item');
+    const circle = document.createElement('i');
+    circle.setAttribute('class','far fa-circle');
+    const input = document.createElement('span');
+    input.textContent = text;
+    const trash = document.createElement('i');
+    trash.setAttribute('class','fas fa-trash-alt');
+    trash.addEventListener('click',() =>{
+        items.removeChild(item);
+    });
+
+    item.appendChild(circle);
+    item.appendChild(input);
+    item.appendChild(trash);
     
+    return item;
+}
+
+
 
